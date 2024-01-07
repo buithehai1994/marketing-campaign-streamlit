b96edfe767dc2328f12204e2bd24a4846a80d1f8
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-01-07 21:54:21 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -170,7 +170,7 @@ elif selected_tab == "EDA":
         with open(eda_report_path, 'r') as file:
             html_content = file.read()
     
-        components.html(html_report, width=800, height=600)
+        components.html(html_content, width=800, height=600)
     if selected_sub_tab == tab_titles[2]:
         sub_tab_titles = ["Graph","Analysis"]
         selected_sub_sub_tab = st.sidebar.radio("Sub-navigation",sub_tab_titles)        
