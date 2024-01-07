4cab415b427b2ee30df8ba72c01483de9c65aa95
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-01-08 09:32:27 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -184,8 +184,12 @@ elif selected_tab == "EDA":
         # Get the HTML content from the Container object
         eda_html = report._repr_html_()
 
-        # Render the HTML content within Streamlit
-        st.components.v1.html(eda_html, width=800, height=600, scrolling=True)
+        # # Render the HTML content within Streamlit
+        # st.components.v1.html(eda_html, width=800, height=600, scrolling=True)
+        # Render the HTML content within an iframe in Streamlit
+        iframe_str = f'<iframe srcdoc="{eda_html}" width=800 height=600></iframe>'
+        st.markdown(iframe_str, unsafe_allow_html=True)
+
     if selected_sub_tab == tab_titles[2]:
         sub_tab_titles = ["Graph","Analysis"]
         selected_sub_sub_tab = st.sidebar.radio("Sub-navigation",sub_tab_titles)        
