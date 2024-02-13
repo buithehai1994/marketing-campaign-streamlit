05daf9bd73747f549050feba49caf106b36e1fc4
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-13 19:22:15 +1100

    Update display.py

--- tab_df/display.py
+++ tab_df/display.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
 def display_tab_df_content(dataset):
-    if dataset.data is None:
-        st.error("No dataset provided or error in loading data")
-        return
-
     # Display original DataFrame
     st.subheader("Original DataFrame:")
     st.write(dataset.head_df())
