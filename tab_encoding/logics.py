c6ce3be8320cc9eb6d8ef80f127d12b8e22f63b8
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-13 16:40:18 +1100

    Update logics.py

--- tab_encoding/logics.py
+++ tab_encoding/logics.py
@@ -10,7 +10,6 @@ class Encoding:
         if self.data is None:
             raise ValueError("No data available. Use set_data() to load data first.")
 
-    @st.cache
     def label_encoding(self):
         if self.data is None:
             raise ValueError("No data available. Use set_data() to load data first.")
