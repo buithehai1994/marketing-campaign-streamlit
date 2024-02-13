2a1bc7a7d62c826f1dd8d0741a8fa42e6d3a36ee
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-13 19:47:35 +1100

    Update display.py

--- tab_df/display.py
+++ tab_df/display.py
@@ -3,4 +3,4 @@ import streamlit as st
 def display_tab_df_content(dataset):
     # Display original DataFrame
     st.subheader("Original DataFrame:")
-    st.write(dataset.head_df())
+    st.write(dataset.head())
