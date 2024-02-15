65bbf36bbdf235773f088233028335d85742637e
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-15 12:03:18 +1100

    Update logics.py

--- tab_ml/logics.py
+++ tab_ml/logics.py
@@ -19,6 +19,16 @@ class ML:
         X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
         X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
         return X_train, X_val, X_test, y_train, y_val, y_test
+
+    def coef(self):
+        if self.trained_model is None:
+            raise ValueError("Model not loaded. Please load the model first.")
+        return self.trained_model.coef_
+
+    def intercept(self):
+        if self.trained_model is None:
+            raise ValueError("Model not loaded. Please load the model first.")
+        return self.trained_model.intercept_
         
     def oversample_data(self, X_train, y_train):
         # Apply SMOTE to balance the class distribution in the training set
