d3f7070bd08e27a48619bfaabd415e7dd17630c2
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-17 19:23:49 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -219,6 +219,7 @@ elif selected_tab == "Machine Learning Model":
         if selected_sub_sub_tab=="Default params":
             # Train the logistic regression model
             ml.train_logistic_regression(X_train, y_train)
+            log_reg=LogisticRegression()
             get_model_metrics(model=log_reg, X_train=X_train, X_val=X_val, X_test=X_test, 
                                                y_train=y_train, y_val=y_val, y_test=y_test)
             
