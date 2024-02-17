85d8d7f903d6eb2f9e72c021df66836879c0a4e4
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-17 21:51:02 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -221,7 +221,7 @@ elif selected_tab == "Machine Learning Model":
             model=LogisticRegression()
             # Train the logistic regression model
             ml=ML()
-            ml.train_model(model, X_train, y_train)
+            trained_model = ml.train_model(model, X_train, y_train)
             get_model_metrics(model=model, X_train=X_train, X_val=X_val, X_test=X_test, 
                                                y_train=y_train, y_val=y_val, y_test=y_test)
             
