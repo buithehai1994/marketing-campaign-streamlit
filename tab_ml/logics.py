e2f846839659ba64c94e8230d2f60ffbf4ec925c
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-17 20:59:42 +1100

    Update logics.py

--- tab_ml/logics.py
+++ tab_ml/logics.py
@@ -1,14 +1,10 @@
 import pickle
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix,precision_recall_fscore_support,roc_curve, auc
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix, roc_curve, auc
 import numpy as np
 import pandas as pd
-import streamlit as st
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-from dataprep.eda import create_report
-from sklearn.linear_model import LogisticRegression
 
 class ML:
     def __init__(self):
@@ -17,9 +13,7 @@ class ML:
         self.scaler = StandardScaler()
 
     def split_data(self, X, y):
-        # Split the data into training (70%), validation (15%), and test (15%) sets
         X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
-        # Split the temporary set (X_temp, y_temp) into validation (50%) and test (50%) sets with stratified sampling
         X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
         return X_train, X_val, X_test, y_train, y_val, y_test
 
@@ -34,79 +28,54 @@ class ML:
         return self.trained_model.intercept_
         
     def oversample_data(self, X_train, y_train):
-        # Apply SMOTE to balance the class distribution in the training set
         X_train_resampled, y_train_resampled = self.smote.fit_resample(X_train, y_train)
         return X_train_resampled, y_train_resampled
     
-    def scale_data(self, X_train,X_test,X_val):
-        # Scale the data
+    def scale_data(self, X_train, X_test, X_val):
         X_train_scaled = self.scaler.fit_transform(X_train)
-        X_test_scaled  = self.scaler.transform(X_test)
-        X_val_scaled  = self.scaler.transform(X_val)
-        return X_train_scaled,X_test_scaled,X_val_scaled
+        X_test_scaled = self.scaler.transform(X_test)
+        X_val_scaled = self.scaler.transform(X_val)
+        return X_train_scaled, X_test_scaled, X_val_scaled
         
     def load_model(self, model_path):
         with open(model_path, 'rb') as file:
             self.trained_model = pickle.load(file)
         
     def calculate_model_metrics(self, X, y):
-
-         # Ensure X is a DataFrame and y is a Series
-        if isinstance(X, pd.DataFrame):
-            X = X.values  # Convert X to a NumPy array
-        if isinstance(y, pd.DataFrame):
-            y = y.squeeze()
-
         if self.trained_model is None:
             raise ValueError("Model not loaded. Please load the model first.")
 
-        # Use the loaded model for predictions and evaluation
         predictions = self.trained_model.predict(X)
 
-        scores = precision_recall_fscore_support(y, predictions, average='weighted')
+        precision, recall, f1, _ = precision_recall_fscore_support(y, predictions, average='weighted')
 
-        return "precision, recall, F1 score:", scores
+        return precision, recall, f1
 
     def calculate_baseline_metrics(self, y):
-        # Convert y to a Pandas Series if it's a DataFrame
-        if isinstance(y, pd.DataFrame):
-            y = y.squeeze()
-
-        # Calculate the majority class in y
-        y_mode = y.mode()[0] if len(y.mode()) > 0 else None
-
-        # Create an array of ones with the same shape as y
+        y_mode = y.mode()[0] if not y.mode().empty else None
         y_base = pd.Series(np.full(y.shape, y_mode), index=y.index)
-
-        # Calculate metrics of the baseline model
         baseline_accuracy = accuracy_score(y, y_base)
-
         return baseline_accuracy
 
     def calculate_confusion_matrix(self, y_true, y_pred):
         return confusion_matrix(y_true, y_pred)
 
-    def calculate_accuracy(self, X,y_true):
-        
-        # Use the loaded model for predictions and evaluation
+    def calculate_accuracy(self, X, y_true):
+        if self.trained_model is None:
+            raise ValueError("Model not loaded. Please load the model first.")
         predictions = self.trained_model.predict(X)
         return accuracy_score(y_true, predictions)
 
     def calculate_roc_curve(self, X, y):
         if hasattr(self.trained_model, "decision_function"):
             y_scores = self.trained_model.decision_function(X)
+            fpr, tpr, _ = roc_curve(y, y_scores)
+            roc_auc = auc(fpr, tpr)
+            return fpr, tpr, roc_auc
         else:
-            # Handle cases where decision_function is not available
             raise AttributeError("Model does not have decision_function method.")
-        fpr, tpr, _ = roc_curve(y, y_scores)
-        roc_auc = auc(fpr, tpr)
-        return fpr, tpr, roc_auc
 
-    def train_model(self, model):
+    def train_model(self, model, X_train, y_train):
         model.fit(X_train, y_train)
-        # Set the trained model to the class attribute
         self.trained_model = model
         return model
-
-    
-            
