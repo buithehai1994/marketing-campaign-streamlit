10cf1e9b5f99e7b499de68ff195d910e5ab41b4c
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-18 22:59:48 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -120,6 +120,22 @@ def dependent_var():
     dependent_var= encoding.splitting_y()
     return dependent_var
     
+@st.cache_resource
+def display_model_evaluation(X_train, X_val, X_test,y_train,y_train_pred, y_val,y_val_pred, y_test,y_test_pred, ml_model):
+    y_train_pred_prob = ml_model.predict_proba(X_train)[:, 1]
+    y_test_pred_prob = ml_model.predict_proba(X_test)[:, 1]
+    y_val_pred_prob = ml_model.predict_proba(X_val)[:, 1]
+    display_model_metrics(X_train, y_train, X_val, y_val, X_test, y_test, ml_model, average='weighted')
+    
+    display_confusion_matrix(y_train, y_train_pred, class_labels=['Not subscribe', 'subscribe'], figsize=(8, 6))
+    display_roc_curve(y_true=y_train, y_scores=y_train_pred_prob, ml_instance=ml_model, title="ROC of Training set")
+    
+    display_confusion_matrix(y_val, y_val_pred, class_labels=['Not subscribe', 'subscribe'], figsize=(8, 6))
+    display_roc_curve(y_true=y_val, y_scores=y_val_pred_prob, ml_instance=ml_model, title="ROC of Validation set")
+    
+    display_confusion_matrix(y_test, y_test_pred, class_labels=['Not subscribe', 'subscribe'], figsize=(8, 6))
+    display_roc_curve(y_true=y_test, y_scores=y_test_pred_prob, ml_instance=ml_model, title="ROC of Testing set")
+    
 # Instantiate the ML class
 ml_instance = ML()
 
